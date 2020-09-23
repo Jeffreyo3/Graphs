a2064042dafa9f9553869661898261245b20dfad
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-09-22 21:21:22 -0700

    complete populate

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -38,15 +41,83 @@ class SocialGraph:
 
         The number of users must be greater than the average number of friendships.
         """
+        
+
         # Reset graph
         self.last_id = 0
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
+        # Calculate max num of friends a user can have
+        # cap_friends = math.ceil(num_users * .25)
+
+        # Calculate total connections between users
+        total_friendships = num_users * avg_friendships
+        # keep track of actual friends added
+        added_count = 0
         # Add users
 
+        for i in range(1, num_users+1):
+            self.add_user(i)
+
         # Create friendships
+        for i in range(1, num_users+1):
+            if added_count < total_friendships:
+                # on odd num users, calculate randomly how many friends they should get
+                if i%2 != 0:
+                    add_friends = random.randint(0, i-1)
+                    
+                    while add_friends > 0:
+                        if added_count >= total_friendships:
+                            add_friends = 0
+                            continue
+                        random_friend = random.randint(1, num_users)
+                        if random_friend != i:
+                            if random_friend not in self.friendships[i]:
+                                self.add_friendship(i, random_friend)
+                                add_friends -= 1
+                                added_count += 2
+                # on even num users, calculate how many friends are needed to keep avg
+                    # randomly generate number below i based on needed friend to keep avg
+                else:
+                    to_add = (avg_friendships * i) - added_count
+                    
+                    if to_add > i:
+                        to_add = i
+
+                    while to_add > 0:
+                        if added_count >= total_friendships:
+                            to_add = 0
+                            continue
+                        random_friend = random.randint(1, num_users)
+                        if random_friend != i:
+                            if random_friend not in self.friendships[i]:
+                                self.add_friendship(i, random_friend)
+                                to_add -= 1
+                                added_count += 2
+
+        # while added_count < total_friendships:
+        #     for i in range(num_users):
+        #         # self.add_user(i+1)
+        #         # print(i)
+        #     # Create friendships
+        #         print(added_count)
+        #         if added_count >= total_friendships:
+        #             continue
+        #         count = 0
+        #         for j in range(1, i):
+        #             # print(j)
+        #             something = round(random.random())
+        #             # print(f"User {i}: {count}, {cap_friends}")
+        #             j_friend = self.friendships[j]
+        #             if something and len(j_friend) < cap_friends:
+        #                 self.add_friendship(i+1, j)
+        #                 count += 1
+        #                 added_count += 1
+        # print(total_friendships, added_count)
+    
+        
 
     def get_all_social_paths(self, user_id):
         """
